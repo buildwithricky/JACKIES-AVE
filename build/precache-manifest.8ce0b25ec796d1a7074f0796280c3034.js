705474bccb5cdbb116fd910c43c63183ec643b3e
ghost-tremble <patokezy@gmail.com>
2021-12-14 21:47:34 +0100

    jjrr

---
+++ build/precache-manifest.8ce0b25ec796d1a7074f0796280c3034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b742a077b67f4b7f6e5f5470d47eb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d96f8322e93003695c9",
+    "url": "/static/css/main.68d42786.chunk.css"
+  },
+  {
+    "revision": "7ed80412956d1a2a6bc3",
+    "url": "/static/js/2.743ecf59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.743ecf59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d96f8322e93003695c9",
+    "url": "/static/js/main.254ef9c0.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
